dc7b035e5670e36dce0c1915d63f3f7143f8972c
Joshua Toulouse <jtoulous@3E5.42angouleme.fr>
2023-05-20 16:09:41 +0200

    weshbb

---
+++ cpp/day_01/ex00/main.cpp
@@ -0,0 +1,14 @@
+#include "Zombie.hpp"
+
+int main(void)
+{
+    Zombie  first("first");
+    Zombie  second("second");
+    Zombie  *ptr;
+
+    first.announce();
+    second.announce();
+    randomChump("random");
+    ptr = newZombie("alloked");
+    delete (ptr);
+}
